3cdc56914194b8a53fc667f0b7181bb71864dee6
loneshark-1 <151842354+loneshark-1@users.noreply.github.com>
2024-05-18 02:06:25 +0000

    Update userInfo.js

--- userInfo.js
+++ userInfo.js
@@ -1,1 +1,9 @@
 
+function createUserProfiles(names, modifiedNames){
+  return names.map((name, i) => ({
+    id: i + 1,
+    originalName: name,
+    modifiedName: modifiedNames[i]
+  }));
+}
+export {createUserProfiles};
